dee6cb9a780452d3a200b03bdc4c01fb5c10563d
pa7lux <pa7lux@gmail.com>
2025-06-20 21:55:57 +0400

    step-4

---
+++ step-4-interactive-frontend/frontend/script.js
@@ -0,0 +1,7 @@
+// Get the button element by its ID
+const button = document.getElementById('clickButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    alert('I am clicked!');
+});
